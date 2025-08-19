9c3b381d329c21c6e90160701fb4ec07af2082cc
ocerlis <56587621+Ocerlis@users.noreply.github.com>
2025-08-19 21:42:28 +0600

    Added mouse trail to grid

--- logic.js
+++ logic.js
@@ -1,12 +1,27 @@
-
+let pixel = document.createElement("div");
 let gridContainer = document.querySelector("#gridContainer"); 
 
-for (i = 1; i <= 16; i++) {
+for (i = 1; i <= 256; i++) {
     let pixel = document.createElement("div");
     pixel.className = "pixelBlock";
     gridContainer.appendChild(pixel);
 }
 
-function mouseHover {
-    
+function mouseTrail() {
+    const pixels = document.querySelectorAll(".pixelBlock");
+
+    pixels.forEach ((pixel) => {
+        pixel.addEventListener('mouseover', function() {
+            pixel.style.backgroundColor = "black";
+            console.log("x");
+        });
+
+        pixel.addEventListener('mouseout', function() {
+            pixel.style.backgroundColor = "gray";
+            console.log("y");
+        });
+    })
 }
+
+mouseTrail();
+
