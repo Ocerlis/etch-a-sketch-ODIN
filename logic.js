b4a1dd79c981df741f6b3ac2be6d3fff29b367d1
ocerlis <56587621+Ocerlis@users.noreply.github.com>
2025-08-19 20:32:29 +0600

    Created test for DOM nodes

---
+++ logic.js
@@ -0,0 +1,11 @@
+
+
+let gridContainer = document.querySelector("#gridContainer"); 
+
+for (i = 1; i <= 16; i++) {
+    let pixel = document.createElement("div");
+    pixel.textContent = "Text!";
+    pixel.className = "pixelBlock";
+    gridContainer.appendChild(pixel);
+}
+
